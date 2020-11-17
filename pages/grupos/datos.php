a24ccb374ec59a8691e8cb9063336bebfd16c955
Octus <figueroagarciamanuel@gmail.com>
2020-11-17 15:14:36 -0600

    Consultas de grupos funcional

---
+++ pages/grupos/datos.php
@@ -0,0 +1,9 @@
+<?php
+    include  '..\..\..\..\Listas\conexion.php';
+    $instruccion = "SELECT G.idGrupo, G.idTaller,T.Nombre,G.Escuela, G.Nombre_Encargado FROM grupos as G INNER JOIN talleres as T ON G.idTaller = T.idTaller ORDER BY G.idGrupo";
+    $sql=mysqli_query($conn,$instruccion);
+    while ($dat = mysqli_fetch_assoc($sql)) {
+        
+        echo "<tr><td>$dat[idGrupo]</td><td>$dat[idTaller]</td><td>".utf8_encode($dat['Nombre'])."</td><td>$dat[Escuela]</td><td>$dat[Nombre_Encargado]</td></tr>";       
+        }
+?>
