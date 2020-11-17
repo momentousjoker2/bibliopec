86ec059f99921263c7bf3f8c02da14915529fea0
Octus <figueroagarciamanuel@gmail.com>
2020-11-17 14:27:45 -0600

    Agregar grupos funcional

---
+++ pages/grupos/grabar.php
@@ -0,0 +1,10 @@
+<?php
+    include  '..\..\..\..\Listas\conexion.php';
+    $idTaller = $_POST['idTaller'];
+    $escuela = $_POST['Escuela'];
+    $encargado = $_POST['Encargado'];
+    $sql = "insert into grupos(idTaller, Escuela, Nombre_Encargado) values ($idTaller, '$escuela','$encargado')";
+        mysqli_query($conn,$sql);
+        mysqli_close($conn);
+        echo "Grupo agregado";
+?>
