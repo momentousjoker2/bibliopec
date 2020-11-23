3836adf60aa4a696676ea7c1c66173fe7f4573b3
Octus <figueroagarciamanuel@gmail.com>
2020-11-23 14:02:37 -0600

    Formulario validado

--- pages/personal/grupos/mUsuario.php
+++ pages/personal/grupos/mUsuario.php
@@ -2,7 +2,7 @@
     include  '..\..\..\..\..\Listas\conexion.php';
     $instruccion = "Select idUsuario, apellidos, nombre From usuarios";
     $sql=mysqli_query($conn,$instruccion);
-    echo '<option selected disabled value="1"></option>';
+    echo '<option selected disabled value=""></option>';
     while ($dat = mysqli_fetch_assoc($sql)) {
         echo "<option value='$dat[idUsuario]'>".utf8_encode($dat['nombre']).' '.utf8_encode($dat['apellidos']).'</option>';       
         }
