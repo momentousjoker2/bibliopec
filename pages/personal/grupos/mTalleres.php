7543b90966b26f1d9dd7826650fb48bc1f95dcd6
Octus <figueroagarciamanuel@gmail.com>
2020-11-22 19:08:26 -0600

    coreccion en grupos

--- pages/personal/grupos/mTalleres.php
+++ pages/personal/grupos/mTalleres.php
@@ -2,6 +2,7 @@
     include  '..\..\..\..\..\Listas\conexion.php';
     $instruccion = "Select idTaller, Nombre From talleres";
     $sql=mysqli_query($conn,$instruccion);
+    echo "<option selected disabled value=''></option> ";
     while ($dat = mysqli_fetch_assoc($sql)) {
         
         echo "<option value='$dat[idTaller]'>".utf8_encode($dat['Nombre']).'</option>';       
