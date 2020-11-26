83934669e3b085301be4ae62823822740c9891f2
Octus <figueroagarciamanuel@gmail.com>
2020-11-26 14:09:51 -0600

    Corecciones

--- pages/personal/grupos/mUsuario.php
+++ pages/personal/grupos/mUsuario.php
@@ -1,9 +1,9 @@
 <?php
     include  '..\..\..\..\..\Listas\conexion.php';
-    $instruccion = "Select idUsuario, apellidos, nombre From usuarios";
+    $instruccion = "Select idUsuario, nombre From usuarios";
     $sql=mysqli_query($conn,$instruccion);
     echo '<option selected disabled value=""></option>';
     while ($dat = mysqli_fetch_assoc($sql)) {
-        echo "<option value='$dat[idUsuario]'>".utf8_encode($dat['nombre']).' '.utf8_encode($dat['apellidos']).'</option>';       
+        echo "<option value='$dat[idUsuario]'>".utf8_encode($dat['nombre']).'</option>';       
         }
 ?>
--- pages/personal/vistas/datos.php
+++ pages/personal/vistas/datos.php
@@ -44,14 +44,14 @@
             break;
         }
     $instruccion ="Select V.idVisitas, V.fecha, V.Hora, V.mConsulta, U.idUsuario, 
-    U.Apellidos, U.Nombre, U.Escolaridad, U.Ocupacion, U.Genero 
+    U.Nombre, U.Escolaridad, U.Ocupacion, U.Genero 
     from visitas As V Inner join USUARIOS AS U ON V.idUsuario = U.idUsuario 
     WHERE MONTH(V.fecha) =" . $mes . " AND YEAR(V.fecha) =".$ann;
     
     $sql=mysqli_query($conn,$instruccion);
     while ($dat = mysqli_fetch_assoc($sql)) {
         
-        echo '<tr><td>'.$dat['idVisitas'].'</td><td>'.$dat['fecha'].'</td><td>'.$dat['Hora'].'</td><td>'.$dat['mConsulta'].'</td><td>'.$dat['idUsuario'].'</td><td>'.$dat['Apellidos'].' '.$dat['Nombre'].'</td><td>'.$dat['Escolaridad'].'</td><td>'.$dat['Ocupacion'].'</td><td>'.$dat['Genero'].'</td></tr>';       
+        echo '<tr><td>'.$dat['idVisitas'].'</td><td>'.$dat['fecha'].'</td><td>'.$dat['Hora'].'</td><td>'.$dat['mConsulta'].'</td><td>'.$dat['idUsuario'].'</td><td>'.$dat['Nombre'].'</td><td>'.$dat['Escolaridad'].'</td><td>'.$dat['Ocupacion'].'</td><td>'.$dat['Genero'].'</td></tr>';       
         }
     } 
 ?>
